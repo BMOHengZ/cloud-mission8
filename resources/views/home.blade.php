aad03a92426e5a59b0bd566383f0da633e952ef9
LYHENG LONG <llong@paragoniu.edu.kh>
2025-01-19 21:18:39 +0700

    view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cloud
+
+    </title>
+</head>
+
+<body>
+    <h1>
+        Hello Cloud Computing Class
+    </h1>
+</body>
+
+</html>
